c7e645681e4879c01033d28402c21d3c609f4b6f
kamleshmohabe1993-ux <kamleshmohabe1993@gmail.com>
2026-07-25 15:29:03 +0530

    public updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,6 @@
+const mongoose = require('mongoose');
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
-
-const mongoose = require('mongoose');
-
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
